f6d1ad29d8a1129e39f31d81eba559a4af9ae41a
Person_1 <maryhennessy@netscape.net>
2016-06-26 14:20:32 +0100

    All the car files
    addcar is working copy of dbs_car_rental
    car_new will be car eventually

--- dbs_car_rental.py
+++ dbs_car_rental.py
@@ -3,88 +3,127 @@
     
     
     
-class  Car_Rental(object):  
+class  CarRental(object):  
 
 
     def __init__(self):
         self.diesel_cars = []
         self.electric_cars = []
         self.hybrid_cars = []
-        self.petrol_cars = []
+        self.petrol_cars = [] #fifo append returns to end - borrow from beginning - keep cars turning over
         
-#fifo append returns to end - borrow from beginning - keep cars turning over
     
+    def rental_transmission_menu(self):
+        print ' 1:  Automatic'
+        print ' 2:  Manual'
+        self.handle_choices()
+        
     
-    def rent_a_car_menu(self):
-    
-    
-    
-        print '      Car Selection Menu\n\n '
+    def rental_fuel_menu(self):
         print ' 1:  Diesel Car'
         print ' 2:  Electric Car'
         print ' 3:  Hybrid Car'
         print ' 4:  Petrol Car'
+        self.handle_choices()
         
+        
+    def rental_size_menu(self):
+        print ' 1: Economy'
+        print ' 2: Mid-range'
+        print ' 3: Large'
+        print ' 4: 7 seater'
+        self.handle_choices()
     
+        
+    def rental_welcome_menu(self):
+        print ' 1: Browse'
+        print ' 2: Rental'  
+        print ' 3: Extend Rental Due Date'
+        print ' 4: Return Car'   
+        self.handle_choices()
+        print 'here'
+        self.rental_welcome_choice()
+       
     
-        q_2 = raw_input('What Kind of Car Do You Want? Enter 1, 2, 3,or 4 from Menu above\n>>')
-        return
-        
-        
-    def greetings(self):
-        
-        q_1 = raw_input('Do you want to rent a car? Enter "y" or "n" \n>>')
-
-        if q_1 == 'y':
-            self.rent_a_car_menu()
+    def rental_welcome_choice(self):
+    
+        if self.handle_choices == 1:
+            self.browse()
+        elif self.greeting == 2:
+            self.rent_a_car()
+        elif self.greeting == 3:
+            self.renew_a_rental()
+        elif self.greeting == 4:
+            self.return_a_car()
             
-        elif q_1 == 'n':
-
-            q_3 = raw_input('Do you want to renew your rental? Enter "y" or "n"\n>>')
+        #else: print 'error'     # not valid choices handled by handle_choices
         
-            if q_3 == 'y':
-                #will change path here - requires Numberplate to find specific car to renew
-                self.rent_a_car_menu()    
+    def browse(self):
+        
+        filehandle = open('carlist.csv')
+        for line in filehandle:
+            carline = line.rstrip()
+            print carline
+        
+    def rental_selection_banner(self):    
+        print  ' Car Fuel  Menu\n\n '
+        print  ' Car Size  Menu\n\n '
+        print  ' Car Transmission  Menu\n\n ' 
+        print  ' Car ' 
+        self.handle_choices()   
+                                
+      
+    def handle_choices(self):  # for use with menus for data entry
+        try:
+            greeting = raw_input('Enter Number(1 or 2 or ... etc) of Required Selection\n\n>') 
+            #print greeting 
+                    
+        except:
+            print 'Invalid Choice'
             
-            elif q_3 == 'n':     
-                q_4 = raw_input('Ok - goodbye - please Enter "y" to start again') 
-                self.farewell()
-        else:
-            print 'Only valid selections are "y" or "n"'
-            return
-    
-    def farewell(self):
-        print 'happy motoring'
+#reads in Cars from excel sheet
+    def import_cars(self):
+        cars={}
 
-rental = Car_Rental()
+        filehandle = open('carlist.csv')
+        for line in filehandle:
+            line = line.rstrip()
+            print line
+            word = line.split(',')
+            fuel = word[1]
+            cars[fuel] = cars.get(fuel,0)+1
+
+        carlist = list()
+        for key,value in cars.items():
+                carlist.append((value, key))
+
+        carlist.sort()
+        print carlist
+        input = raw_input('select car')
+        number = input
+        numbers = ('1','2','3','4','5','6','7','8','9')
+        if isinstance (number, numbers):
+            print number
+            
+
+       
+
 if __name__ == '__main__':
 
-
-    rental.greetings()
     
-    rental.farewell()
-   
+    #words = open('carlist.txt').readlines()
+    #print map(lambda x: x.strip(), words)
     
-        
-        
     
-
-                
-            
-                
-                
-                
-                
-            
-                
-
-
-
-
-
-
-
-
+    carrental = CarRental()
+    
+    while True:
+        #carrental.import_cars()
+        carrental.rental_welcome_menu() 
+        input = raw_input ("Enter 'y' to proceed to Main Menu")      # call Main Menu function
+    
+        if not input =='y' and not input =='Y' and not input == '':  # accept y,Y,return to proceed
+            exit()                                                   # and if not program ends
 
 
 
